d484a62af5899afdf977ec0ad75a749929d72b3c
Sezer Turkdal <sezer.turkdal@hotmail.com>
2023-01-06 23:46:40 +0300

    Model created

---
+++ CryptoApp/Model/CryptoCurrency.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoCurrency.swift
+//  CryptoApp
+//
+//  Created by Sezer on 6.01.2023.
+//
+
+import Foundation
+
+struct CryptoCurrency : Decodable{
+    let currency : String
+    let price : String
+}
